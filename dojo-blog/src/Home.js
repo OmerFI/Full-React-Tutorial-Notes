d74329352dec20c844ccf6dc88e78a84258983e4
OmerFI <omergumushane@hotmail.com>
2021-10-11 16:02:15 +0300

    Add video 10 changes and notes

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,24 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Home = () => {
-  // let name = 'mario';
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  // otomatik olarak event object verilir fonksiyona
-  const handleClick = (e) => {
-    // name = 'luigi';
-    // console.log(name) // luigi yazdırmasına rağmen, template'de update yapmıyor
-    
-    setName('luigi'); // this value is reactive
-    setAge(30);
-  };
-
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome Party", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3},
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
     </div>
   );
 };
--- notes.md
+++ notes.md
@@ -302,3 +302,48 @@ const Home = () => {
 
 export default Home;
 ```
+
+## Video 10 - Outputting Lists
+
+Home.js
+```js
+import { useState } from "react";
+
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome Party", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3},
+  ]);
+  return (
+    <div className="home">
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Home;
+```
+
+index.css (added)
+```css
+/* blog previews / list */
+.blog-preview {
+  padding: 10px 16px;
+  margin: 20px 0;
+  border-bottom: 1px solid #fafafa;
+}
+.blog-preview:hover {
+  box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+}
+.blog-preview h2 {
+  font-size: 20px;
+  color: #f1356d;
+  margin-bottom: 8px;
+}
+```
